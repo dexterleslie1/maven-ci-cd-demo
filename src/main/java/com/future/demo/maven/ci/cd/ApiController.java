80416bc841feeac378f4e69bcec129773b67c0a5
dexterleslie@gmail.com <dexterleslie@gmail.com>
2020-02-23 14:15:20 +0800

    新增status接口，用于检测服务是否就绪

--- src/main/java/com/future/demo/maven/ci/cd/ApiController.java
+++ src/main/java/com/future/demo/maven/ci/cd/ApiController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -27,5 +28,14 @@ public class ApiController {
             HttpServletRequest request,
             HttpServletResponse response){
         return ResponseEntity.ok("This maven project is mainly using for demonstrating CI/CD processes.");
+    }
+
+    /**
+     * 用于检测应用是否准备就绪
+     * @return
+     */
+    @RequestMapping(value = "/status")
+    public ResponseEntity<String> status(){
+        return ResponseEntity.ok("Ready");
     }
 }
