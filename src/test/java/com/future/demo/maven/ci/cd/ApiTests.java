e8df13f56e345f71b598ab88e3c47b3e1d26bcc1
dexterleslie@gmail.com <dexterleslie@gmail.com>
2020-02-23 13:56:47 +0800

    Revert "修改代码测试自动部署是否成功"
    This reverts commit 65e4dd44f038247dac1795c9f09c3a52d7a72a3c.

--- src/test/java/com/future/demo/maven/ci/cd/ApiTests.java
+++ src/test/java/com/future/demo/maven/ci/cd/ApiTests.java
@@ -1,31 +1,31 @@
-//package com.future.demo.maven.ci.cd;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.web.client.RestTemplate;
-//
-///**
-// * @author dexterleslie@gmail.com
-// */
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(
-//        classes={Application.class},
-//        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
-//)
-//public class ApiTests {
-//    @Autowired
-//    private RestTemplate restTemplate = null;
-//
-//    @Test
-//    public void test1(){
-//        ResponseEntity<String> response = this.restTemplate.getForEntity(
-//                "http://localhost:8080/",
-//                String.class);
-//        Assert.assertEquals("This maven project is mainly using for demonstrating CI/CD processes.", response.getBody());
-//    }
-//}
+package com.future.demo.maven.ci.cd;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author dexterleslie@gmail.com
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        classes={Application.class},
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
+)
+public class ApiTests {
+    @Autowired
+    private RestTemplate restTemplate = null;
+
+    @Test
+    public void test1(){
+        ResponseEntity<String> response = this.restTemplate.getForEntity(
+                "http://localhost:8080/",
+                String.class);
+        Assert.assertEquals("This maven project is mainly using for demonstrating CI/CD processes.", response.getBody());
+    }
+}
